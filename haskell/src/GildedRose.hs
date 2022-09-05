efc276fed32c309a44ae4461c2ce0724a880f364
Thomas Thannheiser <TTzuHaus@aol.com>
2022-09-05 23:27:10 +0200

    Refactoring

--- haskell/src/GildedRose.hs
+++ haskell/src/GildedRose.hs
@@ -12,59 +12,34 @@ instance Show Item where
 updateQuality :: GildedRose -> GildedRose
 updateQuality = map updateQualityItem
   where
-    updateQualityItem (Item name sellIn quality) =
-      let
-        quality' =
-          if name /= "Aged Brie"
-             && name /= "Backstage passes to a TAFKAL80ETC concert"
-          then
-            if quality > 0
-            then
-              if name /= "Sulfuras, Hand of Ragnaros"
-              then quality - 1
-              else quality
-            else quality
-          else
-            if quality < 50
-            then
-              quality + 1 +
-                (if name == "Backstage passes to a TAFKAL80ETC concert"
-                 then
-                   if sellIn < 11
-                   then
-                     if quality < 49
-                     then
-                       1 + (if sellIn < 6
-                            then
-                              if quality < 48
-                              then 1
-                              else 0
-                            else 0)
-                     else 0
-                   else 0
-                 else 0)
-            else quality
+    updateQualityItem item@(Item name _ quality) =
+      Item name (decSellIn item) (sellOutFkt item . updateQualityFkt item $ quality)
 
-        sellIn' =
-          if name /= "Sulfuras, Hand of Ragnaros"
-          then sellIn - 1
-          else sellIn
-      in
-        if sellIn' < 0
-        then
-          if name /= "Aged Brie"
-          then
-            if name /= "Backstage passes to a TAFKAL80ETC concert"
-            then
-              if quality' > 0
-              then
-                if name /= "Sulfuras, Hand of Ragnaros"
-                then (Item name sellIn' (quality' - 1))
-                else (Item name sellIn' quality')
-              else (Item name sellIn' quality')
-            else (Item name sellIn' (quality' - quality'))
-          else
-            if quality' < 50
-            then (Item name sellIn' (quality' + 1))
-            else (Item name sellIn' quality')
-        else (Item name sellIn' quality')
+decQuality :: Int -> Int
+decQuality quality = quality - if quality > 0 then 1 else 0
+
+incQuality :: Int -> Int
+incQuality quality = quality + if quality < 50 then 1 else 0
+
+decSellIn :: Item -> Int
+decSellIn (Item name sellIn _) = sellIn - if name == "Sulfuras, Hand of Ragnaros" then 0 else 1
+
+sellOutFkt :: Item -> (Int -> Int)
+sellOutFkt (Item name sellIn _)= 
+  if sellIn < 1 then
+    case name of
+      "Aged Brie" -> incQuality
+      "Backstage passes to a TAFKAL80ETC concert" -> const 0
+      "Sulfuras, Hand of Ragnaros" -> id
+      _ -> decQuality
+  else id
+
+updateQualityFkt :: Item -> (Int -> Int)
+updateQualityFkt (Item name sellIn _) = case name of
+  "Aged Brie" -> incQuality
+  "Backstage passes to a TAFKAL80ETC concert" ->
+    incQuality 
+    . (if sellIn < 11 then incQuality else id)  
+    . (if sellIn < 6 then incQuality else id)
+  "Sulfuras, Hand of Ragnaros" -> id
+  _ -> decQuality
