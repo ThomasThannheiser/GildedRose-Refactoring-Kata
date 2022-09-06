b9b2290f5cf60f9f1023fabfd322d0b47ab46974
Thomas Thannheiser <TTzuHaus@aol.com>
2022-09-06 21:20:26 +0200

    Refactoring

--- haskell/src/GildedRose.hs
+++ haskell/src/GildedRose.hs
@@ -12,34 +12,31 @@ instance Show Item where
 updateQuality :: GildedRose -> GildedRose
 updateQuality = map updateQualityItem
   where
-    updateQualityItem item@(Item name _ quality) =
-      Item name (decSellIn item) (sellOutFkt item . updateQualityFkt item $ quality)
+    updateQualityItem item@(Item name sellIn quality) =
+      if name == "Sulfuras, Hand of Ragnaros" then 
+        item
+      else
+        let sellOutFkt f = if sellIn < 1 then f else id
+        in Item name (sellIn - 1) 
+          ((case name of
+              "Aged Brie" -> 
+                sellOutFkt incQuality 
+                . incQuality
+              "Backstage passes to a TAFKAL80ETC concert" ->
+                sellOutFkt (const 0) .   
+                incQuality 
+                . (if sellIn < 11 then incQuality else id)  
+                . (if sellIn < 6 then incQuality else id)
+              "Conjured Mana Cake" ->
+                sellOutFkt (decQuality . decQuality) 
+                . (decQuality . decQuality)
+              _ -> 
+                sellOutFkt decQuality 
+                . decQuality) 
+           quality)
 
 decQuality :: Int -> Int
 decQuality quality = quality - if quality > 0 then 1 else 0
 
 incQuality :: Int -> Int
 incQuality quality = quality + if quality < 50 then 1 else 0
-
-decSellIn :: Item -> Int
-decSellIn (Item name sellIn _) = sellIn - if name == "Sulfuras, Hand of Ragnaros" then 0 else 1
-
-sellOutFkt :: Item -> (Int -> Int)
-sellOutFkt (Item name sellIn _)= 
-  if sellIn < 1 then
-    case name of
-      "Aged Brie" -> incQuality
-      "Backstage passes to a TAFKAL80ETC concert" -> const 0
-      "Sulfuras, Hand of Ragnaros" -> id
-      _ -> decQuality
-  else id
-
-updateQualityFkt :: Item -> (Int -> Int)
-updateQualityFkt (Item name sellIn _) = case name of
-  "Aged Brie" -> incQuality
-  "Backstage passes to a TAFKAL80ETC concert" ->
-    incQuality 
-    . (if sellIn < 11 then incQuality else id)  
-    . (if sellIn < 6 then incQuality else id)
-  "Sulfuras, Hand of Ragnaros" -> id
-  _ -> decQuality
